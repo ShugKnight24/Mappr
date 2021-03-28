b763b686ff2c488966e11092550bdbc7f9417ae0
Shugmi Shumunov <SShumunov@gmail.com>
2021-03-28 00:24:45 -0400

    Shuffle array and limit results
    Randomize results and improve performance

--- js/main.js
+++ js/main.js
@@ -967,9 +967,28 @@ fetch(externalCities)
 		buildCities(cities)
 	})
 
+// http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
 
 function buildCities(cityArray){
-	cityArray.map(({
+	shuffle(cityArray).slice(0, 125).map(({
 		city,
 		state,
 		latitude,
